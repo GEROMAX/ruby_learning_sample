b3c8af8be4b6b9bc9a0142d3489abcaae6744cf1
GEROMAX <tobita0112@gmail.com>
2018-09-21 09:47:15 +0900

    add sample

---
+++ textile_table_builder.rb
@@ -0,0 +1,39 @@
+class TextileTableBuilder
+  attr_accessor :line_feed_code
+
+  def initialize(data_source, options = {})
+    @line_feed_code = options[:line_feed_code] ||= "\r\n"
+    @data_source = data_source
+  end
+
+  def to_table
+    [create_header].concat(create_rows).join(@line_feed_code)
+  end
+
+  def sorted_keys
+    @sorted_keys ||= @data_source.keys.sort {|a,b| a <=> b}
+  end
+
+  def sorted_keys=(value)
+    @sorted_keys = value
+  end
+
+  def create_header
+    sorted_keys.reduce("|") {|textile, column_name| textile << "_. #{column_name} |" }
+  end
+  private :create_header
+
+  def create_rows
+    rows = []
+    max_row_count = @data_source.values.max.length
+    max_row_count.times do |idx|
+      textile = "|"
+      sorted_keys.each do |key|
+        textile << "^. #{@data_source[key][idx]} |"
+      end
+      rows << textile
+    end
+    rows
+  end
+  private :create_rows
+end
